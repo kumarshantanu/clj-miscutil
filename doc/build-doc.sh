4f1a9c3be84e6d4a873c0ba0a0832b5921f984d3
Shantanu Kumar <kumar.shantanu@gmail.com>
2012-02-25 10:00:05 +0530

    fix indentation and formatting

--- doc/build-doc.sh
+++ doc/build-doc.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
+rm -rf target
 mkdir -p target
-rm -rf target/manual.xml
-asciidoc -b docbook -o target/manual.xml manual.asciidoc
+asciidoc -a pygments -b html -o target/manual.html manual.asciidoc
+asciidoc -a pygments -b docbook -o target/manual.xml manual.asciidoc
 cp by-nd-88x31.png target
 
 rm -rf .lein-plugins
 rm -rf lib
 lein deps && cp lib/dev/docbook-xsl-1.76.1-ns-resources.zip lib/dev/docbook-xsl-1.76.1-ns-resources.jar
 #lein docbkx html
-lein docbkx pdf
-#lein docbkx epub
+lein docbkx pdf epub
 
